48c0c399e3d6e1da787da25e61f0e209de9fdfca
Artem <a.balonin.52@mail.ru>
2022-10-19 16:38:12 +0300

    Решили задачу №1

--- HomeWork01/Program.cs
+++ HomeWork01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдает,
+// какое число больше, а какое меньше.
+// Например:
+// a = 5, b = 7 -> max = 7
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) Console.Write($"{a} больше, {b} меньше");
+else if (a < b) Console.Write($"{a} меньше, {b} больше");
+else Console.Write("Числа равны");
