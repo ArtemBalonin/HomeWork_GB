64192cdab49e25c016d8a184f61385cefe4e2dce
Artem <a.balonin.52@mail.ru>
2022-10-19 18:56:25 +0300

    Решили задачу №4

--- HomeWork04/Program.cs
+++ HomeWork04/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные
+// числа от 1 до N.
+// Пример:
+// 10 -> 2,4,6,8,10
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1)
+    Console.WriteLine("Недопустимое число");
+else
+{
+    int count = num / num;
+    while (count <= num)
+    {
+        if (count % 2 ==0) Console.Write($"{count} ");
+        count++;
+    }
+}
