b93342ae01e94672c820a9940d59e399e4681534
Artem <a.balonin.52@mail.ru>
2022-10-19 18:16:11 +0300

    Решили задачу №3

--- HomeWork03/Program.cs
+++ HomeWork03/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напиши прогрумму, которая на вход принимает число и выдает, является ли число четным
+// Делится ли оно без остатка на 2
+// Пример:
+// 4 -> да
+// -3 -> нет
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) Console.Write("Да");
+    else Console.Write("Нет");
