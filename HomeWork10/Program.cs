4f1360593e8f6713af032c71ce704e0f077a5e74
Artem <a.balonin.52@mail.ru>
2022-10-27 23:13:05 +0300

    Решили задачу № 10

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N) 
+// и выдает таблицу кубов чисел от 1 до (N).
+// 3 -> 1, 8, 27
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void TableCube(int number)
+{
+    if (number > 0)
+    {
+        int index = 1;
+        while (index <= number)
+        {
+            Console.WriteLine($"{index, 4}  {Math.Pow(index, 3)}");
+            index ++;
+        }
+    }
+    else Console.WriteLine("Некорректное число");
+}
+TableCube(num);
