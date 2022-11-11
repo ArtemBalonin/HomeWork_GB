efef95c3c2aa9e78a34b2c0bc5aafcd1787cfe67
Artem <a.balonin.52@mail.ru>
2022-11-11 19:45:19 +0300

    Решили задачу №12

---
+++ HomeWork12/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+
+// Решение с помощью цикла for
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    for (int i = 0; i < num | i > num;)
+    {
+        sum = num % 10 + sum;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int sumDigits = SumDigits(number);
+Console.Write(sumDigits);
+
+// Решение с помощью цикла while
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    while (0 < num | 0 > num)
+    {
+        sum = num % 10 + sum;
+        num = num / 10;
+    }
+    return sum;
+}
+int sumDigits = SumDigits(number);
+Console.Write(sumDigits);
