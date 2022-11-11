19b7179bbb078c65e108a6072b5da5894f6bb86f
Artem <a.balonin.52@mail.ru>
2022-11-11 21:26:36 +0300

    Решили задачу №13

---
+++ HomeWork13/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задает массив из 8 элементов,
+// заполненный псевдослучайными числами и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+int[] CreateArray(int len)
+{
+    int[] arr = new int[len];
+    Random rnd = new Random();  // Вынесли рандом из цикла, так как на мощном компьютере формируются
+                                // одинаковые числа!!!
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(-1000, 1000);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < arr.Length -1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+
+}
+Console.Write("Введите колличество элементов: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+PrintArray(array);
