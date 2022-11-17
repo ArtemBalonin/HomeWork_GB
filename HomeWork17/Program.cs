73f1e7c3d402d5b75ed8199e1190e8d836d10848
Artem <a.balonin.52@mail.ru>
2022-11-17 15:14:57 +0300

    Отредактировал код, по рекомендациям предыдущей HomeWork

--- HomeWork17/Program.cs
+++ HomeWork17/Program.cs
@@ -3,9 +3,6 @@
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223 -> 3
 
-Console.WriteLine("Сколько чисел вы будете вводить? Введите количество: ");
-int quantity = Convert.ToInt32(Console.ReadLine());
-
 int[] UserInputNumbers(int size)
 {
     int[] array = new int[size];
@@ -21,23 +18,26 @@ void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        if(i < array.Length -1) Console.Write($"{array[i]}, ");
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
         else Console.WriteLine($"{array[i]}");
     }
 }
-
-int[] arr = UserInputNumbers(quantity);
-PrintArray(arr);
 
 int QuantityNumMoreZero(int[] array)
 {
     int result = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] > 0) result ++;
+        if (array[i] > 0) result++;
     }
     return result;
 }
+
+Console.WriteLine("Сколько чисел вы будете вводить? Введите количество: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = UserInputNumbers(quantity);
+PrintArray(arr);
 
 int res = QuantityNumMoreZero(arr);
 Console.Write(res);
